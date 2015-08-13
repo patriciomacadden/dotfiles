324f830ee40644ce5de7aa681bcdd12747211e34
Patricio Mac Adden <patriciomacadden@gmail.com>
2015-08-13 00:39:11 -0300

    update path

--- .bash/path.sh
+++ .bash/path.sh
@@ -1,11 +1,12 @@
 # homebrew
 export PATH=/usr/local/bin:$PATH
 
-# rbenv
-export PATH=$HOME/.rbenv/bin:$PATH
-eval "$(rbenv init -)"
+# chruby
+source /usr/local/opt/chruby/share/chruby/chruby.sh
+source /usr/local/opt/chruby/share/chruby/auto.sh
+chruby ruby-2.2.2
 
 # android
-export ANDROID_HOME="/opt/homebrew-cask/Caskroom/android-studio-bundle/0.8.6 build-135.1339820/Android Studio.app/sdk"
+export ANDROID_HOME="/usr/local/Cellar/android-sdk/24.1.2"
 export PATH=$ANDROID_HOME/platform-tools:$ANDROID_HOME/tools:$PATH
 
