da6777b1981eeddc0d0714257140a753c96478d3
Patricio Mac Adden <patriciomacadden@gmail.com>
2015-09-13 19:41:47 -0300

    fix path

--- .bash/path.sh
+++ .bash/path.sh
@@ -1,12 +1,12 @@
 # homebrew
-export PATH=~/bin:/usr/local/bin:/usr/local/sbin:$PATH
+export PATH=/usr/local/bin:/usr/local/sbin:$PATH
+# homebrewed coreutils
+export PATH=/usr/local/opt/coreutils/libexec/gnubin:$PATH
+# local binscripts
+export PATH=~/bin:$PATH
 
 # chruby
 source /usr/local/opt/chruby/share/chruby/chruby.sh
 source /usr/local/opt/chruby/share/chruby/auto.sh
 chruby ruby-2.2.3
-
-# android
-export ANDROID_HOME="/usr/local/Cellar/android-sdk/24.1.2"
-export PATH=$ANDROID_HOME/platform-tools:$ANDROID_HOME/tools:$PATH
 
