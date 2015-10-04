a3a581ae1f681c5a2b16e9590f8f2e9f99d50df1
Patricio Mac Adden <patriciomacadden@gmail.com>
2015-10-04 19:36:35 -0300

    improvements

--- .bash/aliases.sh
+++ .bash/aliases.sh
@@ -12,9 +12,9 @@ alias md='mkdir -p'
 
 # mac os x
 alias dsclean='find . -type f -name .DS_Store -print0 | xargs -0 rm'
-# clean up LaunchServices to remove duplicates in the “Open With” menu
-alias lscleanup="/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user && killall Finder"
+# Clean up LaunchServices to remove duplicates in the “Open With” menu
+alias lscleanup='/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user && killall Finder'
 # Empty the Trash on all mounted volumes and the main HDD
 # Also, clear Apple’s System Logs to improve shell startup speed
-alias emptytrash="sudo rm -rfv /Volumes/*/.Trashes; sudo rm -rfv ~/.Trash; sudo rm -rfv /private/var/log/asl/*.asl"
+alias emptytrash='sudo rm -rfv /Volumes/*/.Trashes; sudo rm -rfv ~/.Trash; sudo rm -rfv /private/var/log/asl/*.asl'
 
