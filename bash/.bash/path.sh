f55848d1b8184abb89e127bdd0217fadaad3f8c5
Patricio Mac Adden <patriciomacadden@gmail.com>
2016-10-22 23:59:19 -0300

    remove unused stuff

--- bash/.bash/path.sh
+++ bash/.bash/path.sh
@@ -1,9 +1,5 @@
 # homebrew
 export PATH=/usr/local/bin:/usr/local/sbin:$PATH
-# homebrewed coreutils
-export PATH=/usr/local/opt/coreutils/libexec/gnubin:$PATH
-# local binscripts
-export PATH=~/bin:$PATH
 
 # chruby
 source /usr/local/opt/chruby/share/chruby/chruby.sh
