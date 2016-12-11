af3a6bc310f5b7d996817d76837cd254e4591660
Patricio Mac Adden <patriciomacadden@gmail.com>
2016-12-11 17:23:06 -0300

    stuff

--- bash/.bash/path.sh
+++ bash/.bash/path.sh
@@ -4,5 +4,5 @@ export PATH=/usr/local/bin:/usr/local/sbin:$PATH
 # chruby
 source /usr/local/opt/chruby/share/chruby/chruby.sh
 source /usr/local/opt/chruby/share/chruby/auto.sh
-chruby ruby-2.3.1
+chruby ruby-2.3.3
 
