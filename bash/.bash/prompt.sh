0e43f072070bc46302a55882080cf50ffcfaa2c1
Patricio Mac Adden <patriciomacadden@gmail.com>
2017-05-03 20:08:30 -0300

    some updates

--- bash/.bash/prompt.sh
+++ bash/.bash/prompt.sh
@@ -1,3 +1,15 @@
-#export PS1='\[$red\]\u \[$white\]at \[$yellow\]\h \[$white\]in \[$green\]\w$(__git_ps1 " \[$white\]on\[$blue\] %s") \[$reset\]❯ '
+bold=`tput bold`
+reset=`tput sgr0`
+
+# Tomorrow Dark colors
+black=`tput setaf 0`
+red=`tput setaf 1`
+green=`tput setaf 2`
+yellow=`tput setaf 3`
+blue=`tput setaf 4`
+magenta=`tput setaf 5`
+cyan=`tput setaf 6`
+white=`tput setaf 7`
+
 export PS1='\[$white\]\w$(__git_ps1 " on %s") \[$blue\]❯\[$reset\] '
 
